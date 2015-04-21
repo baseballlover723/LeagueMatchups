ea5abe84e558b840a2e53d12e766bcfa3feec209
baseballlover723 <rosspa@rose-hulman.edu>
2015-04-21 12:13:44 -0400

    does php work?

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Leaguematchups by baseballlover723</title>
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="javascripts/respond.js"></script>
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" href="stylesheets/ie.css">
+    <![endif]-->
+
+    <link rel="stylesheet" href="stylesheets/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+</head>
+<body>
+<div id="header">
+    <nav>
+        <li class="fork"><a href="https://github.com/baseballlover723/LeagueMatchups">View On GitHub</a></li>
+        <li class="downloads"><a href="https://github.com/baseballlover723/LeagueMatchups/zipball/master">ZIP</a></li>
+        <li class="downloads"><a href="https://github.com/baseballlover723/LeagueMatchups/tarball/master">TAR</a></li>
+        <li class="title">DOWNLOADS</li>
+    </nav>
+</div>
+<!-- end header -->
+
+<div class="wrapper">
+
+    <section>
+        <div id="title">
+            <h1>Leaguematchups</h1>
+            <p>It analyzes matchups for League of Legends</p>
+            <hr>
+            <span class="credits left">Project maintained by <a href="https://github.com/baseballlover723">baseballlover723</a></span>
+            <span class="credits right">Hosted on GitHub Pages &mdash; Theme by <a
+                    href="https://twitter.com/michigangraham">mattgraham</a></span>
+        </div>
+
+        <h3>Welcome to GitHub Pages.</h3>
+        <div id="lane-selector-box">
+            <p>Select your lane:
+                <select id="lane-selector">
+                    <option value="top">Top</option>
+                    <option value="mid">Mid</option>
+                    <option value="adc">ADC</option>
+                    <option value="support">Support</option>
+                    <option value="jungle">Jungle</option>
+                </select>
+            </p>
+        </div>
+
+
+
+
+        <!--[if !IE]>
+        <script>fixScale(document);</script><![endif]-->
+
+</body>
+</html>
